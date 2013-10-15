1ff9f91152e9a8c3d8052c66c78d7f617645bc7f
SamiV <sami.vanhatalo@gmail.com>
2013-10-15 23:17:18 +0300

    Test, adding a Unity3D HelloWorld project.

---
+++ HelloWorld01/Assets/PrintFPS.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class PrintFPS : MonoBehaviour 
+{
+	private float slowestFrame = 0.00001f;
+	private int frameCounter = 0;
+
+	void Start()
+	{
+	    Application.targetFrameRate = 60;
+	}
+	 
+	void OnGUI()
+	{
+		// Reset the slowest frame every 30 frames to see new spikes
+		if( Time.deltaTime > slowestFrame || frameCounter == 180 )
+		{
+			slowestFrame = Time.deltaTime;
+			frameCounter = 0;
+		}
+		
+		GUI.Label(new Rect( 0, 0, Screen.width, Screen.height), System.String.Format("{0:F2} (frame ms)", Time.deltaTime * 1000.0f) );
+		GUI.Label(new Rect( 0, 20, Screen.width, Screen.height), System.String.Format("{0:F2} (slowest ms)", slowestFrame * 1000.0f) );
+		frameCounter++;
+	}
+}
---
+++ HelloWorld01/Assets/RotateObject.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateObject : MonoBehaviour
+{
+	private static string debugText = "Blah";
+	private static int frameCount = 0;
+	
+	void Update()
+	{
+		transform.Rotate(Vector3.right * Time.deltaTime * 30.0f);
+		transform.Rotate(Vector3.up * Time.deltaTime * 50.0f, Space.World);
+	}
+
+	void OnGUI()
+	{
+		frameCount++;
+		
+		if( ( frameCount % 100 ) == 0 )
+			debugText += "1";
+
+		// GUI.Label(new Rect( 0.0f, 0.0f, Screen.width, Screen.height ), debugText );
+	}
+}
